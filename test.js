c85fbf4383652bd425c15af1896769bc615ed9d7
William Van Uitert <82126044+PurpleToaster83@users.noreply.github.com>
2025-05-11 19:49:23 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+        const targetArea = document.getElementByID('element');
+        
+        targetArea.addEventListener('click', function(event) {
+            const x = event.clientX();
+            const y = event.clientY();
+            
+            const rect = targetArea.getBoundingClientRect();
+            
+            if(x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+                console.log("blah");
+            }
+        });
