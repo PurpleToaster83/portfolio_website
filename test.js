b3add2954a514520bbff4a10e1249413f9fbbffc
William Van Uitert <82126044+PurpleToaster83@users.noreply.github.com>
2025-05-25 16:53:55 -0400

    Added a page is tracker

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
+var activePage = 1;
+
 function changePage(page) {
     if(page == 'home') {
         window.location.href = "test.html";
+        activePage = 1;
     }
     else if(page == 'about') {
         window.location.href = "about.html";
+        activePage = 2;
     }
     else if(page == 'projects') {
         window.location.href = "projects.html";
+        activePage = 3;
     }
 }
